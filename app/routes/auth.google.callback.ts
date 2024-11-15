25b801f99f26f9f10a2590e82805bdbdc2a30d32
Andrii Kryvulko <andrii.kryvulko@hotmail.com>
2024-11-15 23:59:50 +0000

    fix: update Google auth callback to redirect to dashboard on success

--- app/routes/auth.google.callback.ts
+++ app/routes/auth.google.callback.ts
@@ -1,10 +1,11 @@
 import { LoaderFunctionArgs } from '@remix-run/node'
 
 import { authenticator } from '~/modules/auth/auth.server'
+import { ROUTE_PATH as DASHBOARD_PATH } from '~/routes/dashboard'
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
   return authenticator.authenticate('google', request, {
-    successRedirect: '/',
+    successRedirect: DASHBOARD_PATH,
     failureRedirect: '/',
   })
 }
