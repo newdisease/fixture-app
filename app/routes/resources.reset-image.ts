bcb3a40674e7c5bde74ad8dc6cacd384be1f1945
Andrii Kryvulko <andrii.kryvulko@hotmail.com>
2024-11-16 13:53:51 +0000

    feat: add routes for image upload and reset functionality

---
+++ app/routes/resources.reset-image.ts
@@ -0,0 +1,12 @@
+import type { ActionFunctionArgs } from '@remix-run/router'
+
+import { requireUser } from '~/modules/auth/auth.server'
+import { prisma } from '~/utils/db.server'
+
+export const ROUTE_PATH = '/resources/reset-image' as const
+
+export async function action({ request }: ActionFunctionArgs) {
+  const user = await requireUser(request)
+  await prisma.userImage.deleteMany({ where: { userId: user.id } })
+  return null
+}
