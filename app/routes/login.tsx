b1b186996e86c09c485a1ad1886e529f7208f9e7
Eugenia <eugeniajelescu@gmail.com>
2025-01-12 23:07:00 -0700

    feat: move login with google to the login page and adjust logic in index.tsx

---
+++ app/routes/login.tsx
@@ -0,0 +1,40 @@
+import { LoaderFunctionArgs, redirect } from '@remix-run/node'
+import { Form } from '@remix-run/react'
+
+import { ThemeSwitcherHome } from '~/components/misc/theme-switcher'
+import { Button, buttonVariants } from '~/components/ui/button'
+import { authenticator } from '~/modules/auth/auth.server'
+import { cn } from '~/utils/misc'
+
+import { ROUTE_PATH as AUTH_GOOGLE_PATH } from './auth.google'
+import { ROUTE_PATH as DASHBOARD_PATH } from './dashboard'
+
+export const ROUTE_PATH = '/login' as const
+export async function loader({ request }: LoaderFunctionArgs) {
+  const sessionUser = await authenticator.isAuthenticated(request)
+
+  if (sessionUser) {
+    throw redirect(DASHBOARD_PATH)
+  }
+  return null
+}
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Login to Fixture App
+          </h1>
+        </header>
+        <Form action={AUTH_GOOGLE_PATH} method="post">
+          <Button className={cn(buttonVariants({ size: 'sm' }), 'h-8')}>
+            Login with Google
+          </Button>
+        </Form>
+        <ThemeSwitcherHome />
+      </div>
+    </div>
+  )
+}
