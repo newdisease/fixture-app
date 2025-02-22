a2ce95271e404806e154b6105f98722fcaa7ce28
Andrii Kryvulko <andrii.kryvulko@hotmail.com>
2025-02-22 23:35:37 +0000

    chore: remove remix-development-tools from dependencies and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { vitePlugin as remix } from '@remix-run/dev'
-import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -11,7 +10,6 @@ declare module '@remix-run/node' {
 
 export default defineConfig({
 	plugins: [
-		remixDevTools(),
 		remix({
 			future: {
 				v3_fetcherPersist: true,
